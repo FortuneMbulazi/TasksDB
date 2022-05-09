da7b6c5edaa66890b0c09d3395f687b125d5def5
FortuneMbulazi <mabasosimphiwe95@gmail.com>
2022-05-09 23:19:43 +0200

    Added functionality for creating a user or person.

--- Tasks/Stored Procedures/CreatePerson.sql
+++ Tasks/Stored Procedures/CreatePerson.sql
@@ -3,8 +3,28 @@
 -- Create date: 08 May 2022
 -- Description:	When called this stored procedure create a task.
 -- =============================================
-CREATE   PROCEDURE CreatePerson
+CREATE PROCEDURE [dbo].[CreatePerson]
+	@Name NVARCHAR(50),
+	@Surname NVARCHAR(50),
+	@Age INT,
+	@Email NVARCHAR(200)
 AS
 BEGIN
-	SELECT 'Hello there...';
+	BEGIN TRY
+		IF EXISTS(SELECT email FROM [dbo].[Person] WHERE email = @Email)
+		BEGIN
+			SELECT 'Email already in use' AS ErrorMessage;
+			RETURN
+		END
+
+		IF(@Name IS NOT NULL AND @Email IS NOT NULL)
+		BEGIN
+			INSERT INTO [dbo].[Person]([Name], Surname, Age, email)
+			VALUES(@Name, @Surname, @Age, @Email);
+		END
+	END TRY
+	BEGIN CATCH
+		SELECT	@@ERROR AS ErrorMessage,
+				-1 StatusCode;
+	END CATCH
 END
